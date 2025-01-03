06db9088bf8f1a45e23b580da2b59ec874cc3002
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2025-01-03 22:11:18 +0530

    Update QuickSort.js

--- QuickSort.js
+++ QuickSort.js
@@ -1,4 +1,3 @@
-
 function QuickSort(A,l,h){
     if(l<h){
         let j=partition(A,l,h);
@@ -8,4 +7,36 @@ function QuickSort(A,l,h){
 }
 
 function partition(A,l,h){
+    let pivot=A[l];
+    let i=l,j=h;
+    
+    while(i<j){
+        do{
+            i++;
+        }while(A[i]<=pivot);
+        
+        do{
+            j--;
+        }while(A[j]>pivot);
+        
+        if(i<j){
+            // swap(A[i],A[j]);
+            let temp=A[j];
+            A[j]=A[i];
+            A[i]=temp;
+        }
+    }
+    
+    // swap(A[j],pivot);
+    let temp=A[j];
+    A[j]=A[l];
+    A[l]=temp;
+
+    return j;
 }
+
+let arr=[2,1,5,7,0];
+QuickSort(arr,0,5);
+console.log(arr);
+
+//OUTPUT: [0,1,2,5,7]
